c84da5ccf103a461a8159eba591814ad4e39edac
ahmetongun <114662826+ahmetongun@users.noreply.github.com>
2022-10-06 14:24:54 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,21 +1,29 @@
 class Otobus:
     """Otobus bilet satis takip sinifi"""
-    def _init_(self,plaka,nereden,nereye,doluKoltuk):
+
+    
+    
+
+    def _init_(self,plaka,nereden,nereye,koltuk):
         self.plaka = plaka
         self.nereden = nereden
         self.nereye = nereye
-        self.doluKoltuk = doluKoltuk
-        class Otobus:
-    """Otobus bilet satis takip sinifi""" 
+        self.koltuk = koltuk
+        self.doluKoltuk = 0
+        self.bosKoltuk = koltuk
+        
+        
+        
+        
+        
     def bilet_sat(self):
         """Otobusteki dolu koltuk sayisini 1 artirir"""
-        self.dolukoltuk -= 1
+        doluKoltuk += 1
         
     def bilet_iade(self):
         """otobusteki dolu koltuk sayisini 1 azaltir"""
-        self.doluKoltuk += 1
+        bosKoltuk -= 1
         
     def durum_yaz(self):
         """otobusun guzergahini, plakasini,bos ve dolu koltuk sayisini yazdirir"""    
-        print(self.nereden,",",self.nereye,",",self.plaka,self.doluKoltuk,",",self.doluKoltuk)
-
+        print(self.nereden,",",self.nereye,",",self.plaka,self.doluKoltuk,",",self.bosKoltuk)
